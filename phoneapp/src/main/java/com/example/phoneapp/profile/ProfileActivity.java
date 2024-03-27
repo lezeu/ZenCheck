e00b306c27e03acea053844e90d3391bda596ce6
lezeu <lezeu.raul@yahoo.com>
2024-03-27 23:09:06 +0200

    nav from dashboard

--- phoneapp/src/main/java/com/example/phoneapp/profile/ProfileActivity.java
+++ phoneapp/src/main/java/com/example/phoneapp/profile/ProfileActivity.java
@@ -4,13 +4,16 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.phoneapp.BaseActivity;
 import com.example.phoneapp.R;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        setupDrawer(R.id.drawer_layout, R.id.design_navigation_view);
     }
 }
